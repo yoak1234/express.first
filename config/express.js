fa1e5c306bfa31c8d4776dc8a5196aba82b3a96b
Nititham <s5535512111@phuket.psu.ac.th>
2017-04-06 19:48:36 +0700

    test simple page

---
+++ config/express.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var path = require('path');
+
+module.exports = function() {
+    var app = express();
+
+    require('../app/routes/index.routes')(app);
+    
+    app.set('views', './app/views');
+    app.set('view engine', 'pug');
+
+    if (process.env.NODE_ENV == 'dev') {
+        app.use(morgan('dev'));
+    } 
+    else {
+        app.use(compression());
+    }
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+
+    app.use('/jquery', express.static('node_modules/jquery/dist/'))
+    // app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+    app.use('/bootstrap', express.static('node_modules/bootstrap/dist/css/'))
+    app.use('/public', express.static('public'))
+
+    return app;
+};
