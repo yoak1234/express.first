494fab40ce0c4ec292ce359679f9823d900d290a
Nititham <s5535512111@phuket.psu.ac.th>
2017-04-06 19:46:03 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-var express = require('express');
+var express = require('./config/express');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 var app = express();
 
-var helloWorld = function(req, res, next) {
-    res.send('Hello World');
-}
-var helloYok = function(req, res, next) {
-    res.send('Hello Yok.');
-}
-
-app.use('/', helloWorld);
-
 app.listen(3000);
-console.log('Server running at http://localhost:3000');
-
 module.exports = app;
+
+console.log('Server running at http://loclahost:3000');
